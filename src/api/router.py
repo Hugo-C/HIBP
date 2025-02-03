3b3dea82610b1150bacd8129a19648231539f8f5
Hugo-C <Hugo-C@users.noreply.github.com>
2025-02-04 00:50:50 +0100

    GH-4 # merge APIPrefix and Prefix types

--- src/api/router.py
+++ src/api/router.py
@@ -1,28 +1,16 @@
-from typing import Annotated
-
-from fastapi import APIRouter, Path
+from fastapi import APIRouter
 from redis import Redis
 
 from src.api.dependencies import SettingsDep
-from src.common import PasswordStorage
+from src.common import Password, PasswordStorage, Prefix
 
 v1_router = APIRouter(
     prefix="/api/v1",
 )
 
-PREFIX_REGEX_PATTERN = "^[0-9a-fA-F]{5}$"
-APIPrefix = Annotated[
-    str,
-    Path(
-        description="5 first chars of a password hash",
-        example="123AB",
-        pattern=PREFIX_REGEX_PATTERN,
-    ),
-]
 
-
-@v1_router.get("/haveibeenrocked/{prefix}")  # TODO add response schema to openAPI
-def check_password_leak(prefix: APIPrefix, settings: SettingsDep):
+@v1_router.get("/haveibeenrocked/{prefix}")
+def check_password_leak(prefix: Prefix, settings: SettingsDep) -> dict[Prefix, set[Password]]:
     redis_client = Redis.from_url(str(settings.kvrocks_url))
     password_storage = PasswordStorage(client=redis_client)
 
--- src/common/types.py
+++ src/common/types.py
@@ -1,3 +1,14 @@
-# type alias for clarity
-Prefix = str  # first 5 chars of password's hash
-Password = str
+from typing import Annotated
+
+from fastapi import Path
+
+PREFIX_REGEX_PATTERN = "^[0-9a-fA-F]{5}$"  # first 5 chars of password's hash
+Prefix = Annotated[
+    str,
+    Path(  # /!\ only enforced on FastApi side
+        description="5 first chars of a password hash",
+        example="5c283",
+        pattern=PREFIX_REGEX_PATTERN,
+    ),
+]
+Password = str  # type alias for clarity
