08a62972041594de69d8c5c5ef75ad3be7c954dc
Hugo-C <Hugo-C@users.noreply.github.com>
2025-02-03 23:46:47 +0100

    GH-4 # split api's main.py and add openAPI redirect

--- src/api/main.py
+++ src/api/main.py
@@ -1,26 +1,12 @@
-from typing import Annotated
+from fastapi import FastAPI
+from starlette.responses import RedirectResponse
 
-from fastapi import APIRouter, FastAPI, Path
-
-v1_router = APIRouter(
-    prefix="/api/v1",
-)
-
-PREFIX_REGEX_PATTERN = "^[0-9a-fA-F]{5}$"
-APIPrefix = Annotated[
-    str,
-    Path(
-        description="5 first chars of a password hash",
-        example="123AB",
-        pattern=PREFIX_REGEX_PATTERN,
-    ),
-]
-
-
-@v1_router.get("/haveibeenrocked/{prefix}")
-def check_password_leak(prefix: APIPrefix):
-    return {"Hello": "World"}
-
+from .router import v1_router
 
 app = FastAPI()
 app.include_router(v1_router)
+
+
+@app.get("/")
+async def root() -> RedirectResponse:
+    return RedirectResponse(url="/docs")
---
+++ src/api/router.py
@@ -0,0 +1,22 @@
+from typing import Annotated
+
+from fastapi import APIRouter, Path
+
+v1_router = APIRouter(
+    prefix="/api/v1",
+)
+
+PREFIX_REGEX_PATTERN = "^[0-9a-fA-F]{5}$"
+APIPrefix = Annotated[
+    str,
+    Path(
+        description="5 first chars of a password hash",
+        example="123AB",
+        pattern=PREFIX_REGEX_PATTERN,
+    ),
+]
+
+
+@v1_router.get("/haveibeenrocked/{prefix}")
+def check_password_leak(prefix: APIPrefix):
+    return {"Hello": "World"}
