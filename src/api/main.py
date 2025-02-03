0d334759d71a3c884336f5405db9210b5d2d6bd4
Hugo-C <Hugo-C@users.noreply.github.com>
2025-02-03 23:43:18 +0100

    GH-4 # add check_password_leak validation

--- src/api/main.py
+++ src/api/main.py
@@ -1,8 +1,26 @@
-from fastapi import FastAPI
+from typing import Annotated
+
+from fastapi import APIRouter, FastAPI, Path
+
+v1_router = APIRouter(
+    prefix="/api/v1",
+)
+
+PREFIX_REGEX_PATTERN = "^[0-9a-fA-F]{5}$"
+APIPrefix = Annotated[
+    str,
+    Path(
+        description="5 first chars of a password hash",
+        example="123AB",
+        pattern=PREFIX_REGEX_PATTERN,
+    ),
+]
+
+
+@v1_router.get("/haveibeenrocked/{prefix}")
+def check_password_leak(prefix: APIPrefix):
+    return {"Hello": "World"}
+
 
 app = FastAPI()
-
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+app.include_router(v1_router)
--- tests/integration/api/test_api.py
+++ tests/integration/api/test_api.py
@@ -5,7 +5,22 @@ from src.api.main import app
 client = TestClient(app)
 
 
-def test_main():
-    response = client.get("/")
-    assert response.status_code == 200
-    assert response.json() == {"Hello": "World"}
+def test_api_return_404_on_missing_prefix():
+    response = client.get("/api/v1/haveibeenrocked/")
+    assert response.status_code == 404
+
+
+def test_api_return_400_on_prefix_too_short():
+    response = client.get("/api/v1/haveibeenrocked/123")
+    assert response.status_code == 422
+    assert "detail" in response.json()  # error message is quite specific so we don't assert it
+
+
+def test_api_return_400_on_prefix_too_long():
+    response = client.get("/api/v1/haveibeenrocked/123456")
+    assert response.status_code == 422
+
+
+def test_api_return_400_on_prefix_not_hexadecimal():
+    response = client.get("/api/v1/haveibeenrocked/12xyz")
+    assert response.status_code == 422
