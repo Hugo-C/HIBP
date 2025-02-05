cdbbf10ab601f8ead6efe054ecf947ffa71ff325
Hugo-C <Hugo-C@users.noreply.github.com>
2025-02-05 23:52:17 +0100

    GH-1 # add GUI input to parametrize password generation

--- src/static/interface_password_generation.js
+++ src/static/interface_password_generation.js
@@ -7,10 +7,40 @@ document.getElementById('generatePasswordButton').addEventListener('click', (e) 
 })
 
 function generatePasswordForUI() {
-    let password = generatePassword(10);
+    let length = document.getElementById('passwordLengthInput').value;
+    let min_lowercase = 0;  // option not exposed to the user
+    let has_uppercase = document.getElementById('hasUpperCaseInput').checked;
+    let min_uppercase = parseInt(document.getElementById('minUpperCaseInput').value);
+
+    let has_number = document.getElementById('hasNumberInput').checked;
+    let min_number = parseInt(document.getElementById('minNumberInput').value);
+
+    let has_special_char = document.getElementById('hasSpecialCharInput').checked;
+    let min_special_char = parseInt(document.getElementById('minSpecialCharInput').value);
+
+    // Weak input verification
+    if (min_lowercase + min_uppercase + min_number + min_special_char > length) {
+        displayPasswordGenerationInvalidChoice();
+        return;
+    }
+
+    let password = generatePassword(
+        length,
+        min_lowercase,
+        has_uppercase,
+        min_uppercase,
+        has_number,
+        min_number,
+        has_special_char,
+        min_special_char,
+    );
     displayPasswordGenerationResult(password)
 }
 
 function displayPasswordGenerationResult(password){
     passwordGenerationResult.innerHTML = `Mot de passe généré: <div id="passwordGenerated">${password}</div>`
+}
+
+function displayPasswordGenerationInvalidChoice(){
+    passwordGenerationResult.innerHTML = '❌ Les paramètres ne sont pas cohérent'
 }
